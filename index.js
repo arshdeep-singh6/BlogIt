ee378d2c837478762646456bd57b9f6b96b13a00
arshdeep-singh6 <sin14582@sheridancollege.ca>
2025-03-14 13:11:03 -0400

    changes for deployment

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from "express";
 const app = express();
 
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 let posts = [];
 let postId = 0;
 
@@ -82,7 +82,7 @@ app.post("/edit", (req, res) =>
 });
 
 app.listen(port, ()=>{
-    console.log(`Server is listening on port ${port}.`);
+    console.log(`Server is listening on port ${PORT}.`);
 });
 
 
