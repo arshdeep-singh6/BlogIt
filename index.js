a6bb720c701a04326946722bdff26596cf81c91f
arshdeep-singh6 <sin14582@sheridancollege.ca>
2025-03-14 13:31:01 -0400

    file path changes using dirname and fileURLToPATH modules

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 import express from "express";
-const path = require("path");
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
 const app = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const PORT = process.env.PORT || 3000;
 let posts = [];
